49721820943cef71b5c6431448f530437c9e63d0
Krzysztof Augustyniak <office@pkproject.pl>
2021-11-30 14:45:04 +0100

    photo corrections

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-const header = document.getElementById('header');
-const toggle = document.getElementById('toggle');
-const navbar = document.getElementById('navbar');
+//początek od różnych slajdów
+var numer = Math.floor(Math.random()*12)+1;
 
-document.onclick = function (e) {
-    if (e.target.id !== 'header' && e.target.id !== 'toggle'
-        && e.target.id !== 'navbar') {
-        toggle.classList.remove('active');
-        navbar.classList.remove('active');
-    }
+//wyzerowanie czasu
+var timer1 = 0
+var timer2 = 0
+
+//
+function ustawslajd(nrslajdu)
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nrslajdu -1;
+
+    schowaj();
+    setTimeout("zmienslajd()", 500);
 }
 
-toggle.onclick = function () {
-    toggle.classList.toggle('active');
-    navbar.classList.toggle('active');
+function schowaj()
+{
+    $("#slider").fadeOut(500);
+}
+
+// zmiana slajdu na następny
+function zmienslajd()
+{
+    numer++; if (numer>12) numer=1
+
+    //ścieżka do galerii
+    var plik = "<img src=\"/images/Work/Slajd" + numer +".jpg\"/>";
+    
+    document.getElementById("slider").innerHTML = plik
+    $("#slider").fadeIn(500);
+
+    timer1 = setTimeout("zmienslajd()",5000);
+    timer2 = setTimeout("schowaj()",4500);
 }
