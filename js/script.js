940a4a2baefaad2ab2254e252869d5ef1f8ddcc2
Paula111 <paulina.babinczuk@gmail.com>
2021-12-01 14:24:05 +0100

    add changes in js file

--- js/script.js
+++ js/script.js
@@ -1,37 +1,56 @@
+
+//ustawienia hamburgera
+const header = document.getElementById('header');
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('navbar');
+
+document.onclick = function (e) {
+    if (e.target.id !== 'header' && e.target.id !== 'toggle'
+        && e.target.id !== 'navbar') {
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+    }
+}
+
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
+
 //początek od różnych slajdów
-var numer = Math.floor(Math.random()*12)+1;
+var number = Math.floor(Math.random()*12)+1;
 
 //wyzerowanie czasu
 var timer1 = 0
 var timer2 = 0
 
 //
-function ustawslajd(nrslajdu)
+function setphoto(nrphoto)
 {
     clearTimeout(timer1);
     clearTimeout(timer2);
-    numer = nrslajdu -1;
+    number = nrphoto -1;
 
-    schowaj();
-    setTimeout("zmienslajd()", 500);
+    hide();
+    setTimeout("changephoto()", 500);
 }
 
-function schowaj()
+function hide()
 {
     $("#slider").fadeOut(500);
 }
 
 // zmiana slajdu na następny
-function zmienslajd()
+function changephoto()
 {
-    numer++; if (numer>12) numer=1
+    number++; if (number>12) number=1
 
     //ścieżka do galerii
-    var plik = "<img src=\"/images/Work/Slajd" + numer +".jpg\"/>";
+    var plik = "<img src=\"/images/Work/Slajd" + number +".jpg\"/>";
     
     document.getElementById("slider").innerHTML = plik
     $("#slider").fadeIn(500);
 
-    timer1 = setTimeout("zmienslajd()",5000);
-    timer2 = setTimeout("schowaj()",4500);
+    timer1 = setTimeout("changephoto()",5000);
+    timer2 = setTimeout("hide()",4500);
 }
